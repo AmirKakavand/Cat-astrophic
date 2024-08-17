1d7695900063bf234400b05c23c87fc1142c760b
Amir Kakavand <amirkkvnd99@gmail.com>
2024-08-17 19:06:10 +0330

    Added basePath and assetPrefix in the next.config file to fix the 404 error on github pages deployment because of the dynamic routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'cdn2.thecatapi.com',
-            port: '',
-            pathname: '/images/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn2.thecatapi.com",
+        port: "",
+        pathname: "/images/**",
       },
+    ],
+  },
+  basePath: "/Cat-astrophic",
+  assetPrefix: "/Cat-astrophic/",
 };
 
 export default nextConfig;
